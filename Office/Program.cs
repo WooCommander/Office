12f06fe29999e02eef7b4817e477f0be3189ffc9
teacher <teacher@302_04>
2017-09-26 12:51:11 +0300

    Add project files.

---
+++ Office/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
